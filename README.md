cabfb6ca39017d33dd6176ba44a1c5fd7d31af39
Lily Chon <lchon@perkinscoie.com>
2019-10-19 16:59:59 -0700

    Added week 5 homework

--- README.md
+++ README.md
@@ -1,88 +1,59 @@
-# Unit 04 Web APIs Homework: Code Quiz
+# Unit 05 Third-Party APIs Homework: Day Planner
 
-As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment. These assessments are typically a combination of multiple-choice questions and interactive challenges. In this homework assignment, your challenge is to build a code quiz with multiple-choice questions.
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
+The app should display standard business hours (9 a.m. to 5 p.m.). Each time slot should represent one hour and contain the following:
 
-## Instructions
+* The time
 
-From scratch, build a timer-based quiz application that stores high scores client-side. Following the [common templates for user stories](https://en.wikipedia.org/wiki/User_story#Common_templates), we can frame this challenge as follows:
+* A field to hold user input
 
-```
-As a coding bootcamp student
-I want to take a timed quiz on JavaScript fundamentals that stores high scores
-so that I can gauge my progress compared to my peers
-```
+* A save button
 
-How do you deliver this? Here are some guidelines:
+Clicking on the save button will store the time and user input in `localStorage`.
 
-* Play proceeds as follows:
+Near the top of the calendar, the application should display the current day. Additionally, each hour should be color coded to reflect whether the time slot is in the past, the present, or the future. This will change depending on the time of day.
 
-  * The user arrives at the landing page and is presented with a call-to-action to "Start Quiz." Also note the navigation option to "View Highscores" and the "Time" value set at 0.
+You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
 
-  * Clicking the "Start Quiz" button presents the user with a series of questions. The timer is initialized with a value and immediately begins countdown.
+![day planner demo](./Assets/05-Third-Party-APIs-homework-demo.gif)
 
-  * Score is calculated by time remaining. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalty (for example, 15 seconds are subtracted from time remaining).
+## User Story
 
-  * When time runs out and/or all questions are answered, the user is presented with their final score and asked to enter their initials. Their final score and initials are then stored in `localStorage`.
+AS AN employee with a busy schedule
 
-* Your application should also be responsive, ensuring that it adapts to multiple screen sizes.
+I WANT to add important events to a daily planner
 
-* Refer to the animated GIF below for a demonstration of the application functionality.
+SO THAT I can manage my time effectively 
 
-![code quiz](./Assets/04-Web-APIs-homework-demo.gif)
+## Business Context
 
-
-### Hints
-
-* Store your questions as an array of objects in a separate file, `questions.js`, that follows this format:
-
-```js
-var questions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-  },
-  ///etc.
-];
-```
-
-* The length of the array in `questions.js` determines the length of play. Fifteen seconds per question is a good estimate, so 5 questions will result in a length of play of 75 seconds.
-
+Poor time management can result in missed meetings and deadlines or create the appearance of unprofessionalism. A daily planner allows employees to see their day at a glance, schedule time effectively, and improve productivity. 
 
 ## Minimum Requirements
 
 * Functional, deployed application.
 
-* GitHub repository with README describing project.
+* GitHub repository with a unique name and a README describing project.
 
-* The first view of the application displays a button that starts the quiz.
+* The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).
 
-* Clicking the start button displays a series of questions.
+* Each timeblock contains an input field and save button.
 
-* Once the quiz begins, a timer starts.
+* Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed.
 
-* If a question is answered incorrectly, additional time is subtracted from the timer.
+* The current day is displayed at the top of the calendar.
 
-* The timer stops when all questions have been answered or the timer reaches 0.
+* Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
 
-* After the game ends, the user can save their initials and score to a highscores view using local storage.
+```
+GIVEN that an employee adds events to a specific hour in a calendar
 
-## Bonus
+WHEN the employee clicks the save button
 
-* Add audio files to alert the user of correct or incorrect answers. Be sure to include the appropriate license.
-
-* Customize the application theme.
-
-* Create multiple quizzes and an option for users to choose between them.
-
-* Add the application to your portfolio.
-
+THEN events are saved in the timeblock for that hour
+```
+- - -
 
 ## Commit Early and Often
 
@@ -104,7 +75,6 @@ Follow these guidelines for committing:
 
 We would like you to have well over 200 commits by graduation, so commit early and often!
 
-
 ## Submission on BCS
 
 You are required to submit the following:
@@ -112,7 +82,6 @@ You are required to submit the following:
 * The URL of the deployed application
 
 * The URL of the GitHub repository
-
 
 - - -
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- script.js
+++ script.js
@@ -1,204 +1,71 @@
-var viewH = document.querySelector("#viewH");
-var viewT = document.querySelector("#viewT");
-var start = document.querySelector("#start");
-var timer = document.querySelector("#timer");
-var content = document.querySelector("#content");
+$(document).ready(function () {
+    const currentDate = moment().format("dddd, MMMM Do");
 
-var test = false;
-var score = 0;
-var timerInterval;
-// const lastQuestion = questions.length - 1;
-var currentQuestion = 0;
-var secondsLeft = 75;
+    $("#currentDay").html(currentDate);
 
-function display_start() {
-  var h1El = document.createElement("h1");
-  h1El.textContent = "Coding Quiz Challenge";
-  content.append(h1El);
 
-  var h3El = document.createElement("h3");
-  h3El.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
-  content.append(h3El);
+    var times = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+    const currentTime = moment().format("H");
+    var schedule = {}
 
-  var buttonEl = document.createElement("button");
-  buttonEl.textContent = "Start Quiz";
-  content.append(buttonEl);
+    function buildRows() {
 
-  buttonEl.addEventListener("click", function () {
-    content.removeChild(h1El);
-    content.removeChild(h3El);
-    content.removeChild(buttonEl);
-    startQuiz();
-    setTime();
-  });
-}
+        var startTime = 8;
 
-function setTime() {
-  timerInterval = setInterval(function () {
-    secondsLeft--;
-    timer.textContent = secondsLeft;
+        for (var i = 0; i < times.length; i++) {
 
-    if (secondsLeft === 0) {
-      clearInterval(timerInterval);
+            var row = $("<div class='row'>");
+
+            var time = $("<div>");
+            time.attr("data-timeOption", times[i]);
+            time.text(times[i]);
+            time.addClass("col-md-1 hour");
+
+            var textArea = $("<textarea/>");
+            textArea.attr("id", startTime++);
+            textArea.addClass("userInput col-md row");
+
+
+            if (startTime == currentTime) {
+                textArea.addClass("present")
+            } else if (startTime > currentTime) {
+                textArea.addClass("future")
+            } else {
+                textArea.addClass("past")
+            }
+
+            var saveBtn = $("<button>");
+            saveBtn.addClass("col-md-1 fas fa-save saveBtn");
+
+            row.append(time, textArea, saveBtn);
+
+            $(".container").append(row);
+
+        }
+
+        schedule = JSON.parse(localStorage.getItem("getValue"))
+        console.log(schedule)
+        for (key in schedule) {
+            console.log(key, schedule[key])
+            $("#" + key).text(schedule[key])
+        }
+
+        $(".saveBtn").on("click",
+            function (event) {
+                event.preventDefault();
+                console.log(this)
+                var userInput = $(this).parent().find(".userInput").val();
+                var hour = $(this).parent().find(".userInput").attr("id")
+                console.log(hour, userInput);
+                var scheduleTemp = JSON.parse(localStorage.getItem("getValue"))
+                if (scheduleTemp === null) {
+                    scheduleTemp = {}
+                }
+                scheduleTemp[hour] = userInput
+                localStorage.setItem("getValue", JSON.stringify(scheduleTemp));
+            })
     }
 
-  }, 1000);
-}
+    buildRows();
 
-function startQuiz() {
-  content.innerHTML = "";
-  var h2El = document.createElement("h2");
-  h2El.setAttribute("class", "question");
-  h2El.setAttribute("question-index", currentQuestion);
-  h2El.textContent = questions[currentQuestion].title;
-  content.append(h2El);
-
-  for (var j = 0; j < questions[currentQuestion].choices.length; j++) {
-    var buttonEl = document.createElement("button");
-    buttonEl.setAttribute("id", "buttons");
-    buttonEl.setAttribute("choice-index", j);
-    buttonEl.textContent = j + 1 + ". " + questions[currentQuestion].choices[j];
-    content.append(buttonEl);
-
-    buttonEl.addEventListener("click", function (event) {
-      var btnChoice = event.target;
-      var h2Question = document.querySelector(".question");
-      var choiceIndex = parseInt(btnChoice.getAttribute("choice-index"));
-      var question = questions[h2Question.getAttribute("question-index")];
-      if (choiceIndex === question.answer) {
-        var hrEl = document.createElement("hr")
-        var pEl = document.createElement("p");
-        pEl.textContent = "Correct!";
-        content.append(hrEl);
-        content.append(pEl);
-        score++
-      } else {
-        var hrEl = document.createElement("hr")
-        var pEl = document.createElement("p");
-        pEl.textContent = "Wrong!";
-        content.append(hrEl);
-        content.append(pEl);
-        // score--
-        secondsLeft -= 10
-      }
-      currentQuestion++
-      if (currentQuestion > questions.length - 1) {
-        showResult();
-        // clearInterval(timerInterval);
-      } else {
-        setTimeout(startQuiz, 1000);
-      }
-    })
-  }
-}
-
-function stopTime() {
-  if (test) { }
-  secondsLeft = 0;
-  clearInterval(timerInterval);
-}
-
-function showResult() {
-  content.innerHTML = "";
-  stopTime();
-
-  viewT.setAttribute("style", "visibility: hidden;");
-
-  var h2El = document.createElement("h2");
-  h2El.textContent = "All Done!";
-
-  var yourScore = document.createElement("p");
-  yourScore.textContent = " Your score is " + score;
-
-  var par = document.createElement("p");
-
-  var initialsLabel = document.createElement("label");
-  initialsLabel.setAttribute("for", "userInitials");
-  initialsLabel.textContent = "Enter Initials:   ";
-
-  var initialsInput = document.createElement("input");
-  initialsInput.setAttribute("id", "userInitials");
-  initialsInput.setAttribute("name", "userInitials");
-  initialsInput.setAttribute("minlength", "20");
-  initialsInput.setAttribute("maxlength", "20");
-  initialsInput.setAttribute("size", "20");
-
-  var submit = document.createElement("button");
-  submit.setAttribute("class", "submitBtn");
-  submit.textContent = "Submit";
-
-  content.appendChild(h2El);
-  content.appendChild(yourScore);
-  content.appendChild(initialsLabel);
-  content.appendChild(initialsInput);
-  content.appendChild(par);
-  content.appendChild(submit);
-
-  submit.addEventListener("click", function () {
-    // let thisScore = [ { name: initialsInput.value, score: score } ];
-
-    var highScore = JSON.parse(localStorage.getItem("highScore"));
-    if (!highScore) {
-      highScore = {}
-    }
-    var myScoreStr = JSON.stringify(highScore);
-    localStorage.setItem('highScore', myScoreStr);
-    showHighScores();
-  });
-}
-
-function showHighScores() {
-  content.innerHTML = "";
-  timer.setAttribute("style", "visibility: hidden;");
-
-  var highScore = JSON.parse(localStorage.getItem("highScore"));
-
-  var heading = document.createElement("h2");
-  heading.setAttribute("id", "main-heading");
-  heading.textContent = "Top 5 High Scores";
-
-  content.appendChild(heading);
-  
-
-  if ( highScore !== null ) {
-    // highScore.sort((a,b) => (a.score < b.score) ? 1: -1);
-
-    var numScores2Display = 5;
-    if ( highScore.length < 5 ) { 
-      numScores2Display = highScore.length; 
-    }
-
-    for (var i = 0; i < numScores2Display; i++) {
-      var s = highScore[i];
-
-      var p = document.createElement("p");
-      // p.textContent = s.name + " " + s.score + " ( " + s.type + " )";
-      p.textContent = "SCORE HERE"
-      content.appendChild(p);
-    }
-  } else {
-    var p = document.createElement("p");
-    p.textContent =  "Your Initials Here!"
-    content.appendChild(p);
-  }
-
-
-  // creates button to start the game
-  var clear = document.createElement("button");
-  clear.setAttribute("id", "clear");
-  clear.setAttribute("class", "clearBtn");
-  clear.textContent = "Clear!";
-
-  content.appendChild(clear);
-
-  clear.addEventListener("click", function() {
-    localStorage.clear();
-    p.innerHTML = ""
-  });
-}
-
-viewH.addEventListener("click", function() {
-  showHighScores();
 })
-
-display_start();
